7e5e3965448ebba44a09596f44c21e23b5f97d20
Daniell Plasmmer <daniellmesquito@gmail.com>
2016-12-15 23:52:46 -0200

    Updates

---
+++ cordova/platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,590 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-ace/www/Extensions.js",
+        "id": "cordova-plugin-ace.Extensions",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.Extensions"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/ToNative.js",
+        "id": "cordova-plugin-ace.ToNative",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.ToNative"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/ace.js",
+        "id": "cordova-plugin-ace.ace",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/external.js",
+        "id": "cordova-plugin-ace.external",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.external"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/platformHelpers/android.js",
+        "id": "cordova-plugin-ace.android",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.android"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/platformHelpers/ios.js",
+        "id": "cordova-plugin-ace.ios",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.ios"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/primitives/NativeObject.js",
+        "id": "cordova-plugin-ace.NativeObject",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.NativeObject"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/primitives/KnownNativeObject.js",
+        "id": "cordova-plugin-ace.KnownNativeObject",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.KnownNativeObject"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/primitives/WrappedNativeObject.js",
+        "id": "cordova-plugin-ace.WrappedNativeObject",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.WrappedNativeObject"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/primitives/UIElement.js",
+        "id": "cordova-plugin-ace.UIElement",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.UIElement"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/primitives/Control.js",
+        "id": "cordova-plugin-ace.Control",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.Control"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/primitives/ContentControl.js",
+        "id": "cordova-plugin-ace.ContentControl",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.ContentControl"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/primitives/UserControl.js",
+        "id": "cordova-plugin-ace.UserControl",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.UserControl"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/primitives/ItemsControl.js",
+        "id": "cordova-plugin-ace.ItemsControl",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.ItemsControl"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/primitives/Selector.js",
+        "id": "cordova-plugin-ace.Selector",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.Selector"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/primitives/SelectorItem.js",
+        "id": "cordova-plugin-ace.SelectorItem",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.SelectorItem"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/collections/Dictionary.js",
+        "id": "cordova-plugin-ace.Dictionary",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.Dictionary"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/collections/ObservableCollection.js",
+        "id": "cordova-plugin-ace.ObservableCollection",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.ObservableCollection"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/collections/ColumnDefinitionCollection.js",
+        "id": "cordova-plugin-ace.ColumnDefinitionCollection",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.ColumnDefinitionCollection"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/collections/CommandBarElementCollection.js",
+        "id": "cordova-plugin-ace.CommandBarElementCollection",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.CommandBarElementCollection"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/collections/InlineCollection.js",
+        "id": "cordova-plugin-ace.InlineCollection",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.InlineCollection"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/collections/ItemCollection.js",
+        "id": "cordova-plugin-ace.ItemCollection",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.ItemCollection"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/collections/PathFigureCollection.js",
+        "id": "cordova-plugin-ace.PathFigureCollection",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.PathFigureCollection"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/collections/PathSegmentCollection.js",
+        "id": "cordova-plugin-ace.PathSegmentCollection",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.PathSegmentCollection"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/collections/ResourceDictionary.js",
+        "id": "cordova-plugin-ace.ResourceDictionary",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.ResourceDictionary"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/collections/RowDefinitionCollection.js",
+        "id": "cordova-plugin-ace.RowDefinitionCollection",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.RowDefinitionCollection"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/collections/SetterBaseCollection.js",
+        "id": "cordova-plugin-ace.SetterBaseCollection",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.SetterBaseCollection"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/collections/UIElementCollection.js",
+        "id": "cordova-plugin-ace.UIElementCollection",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.UIElementCollection"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/panels/Panel.js",
+        "id": "cordova-plugin-ace.Panel",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.Panel"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/panels/Canvas.js",
+        "id": "cordova-plugin-ace.Canvas",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.Canvas"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/panels/Grid.js",
+        "id": "cordova-plugin-ace.Grid",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.Grid"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/panels/StackPanel.js",
+        "id": "cordova-plugin-ace.StackPanel",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.StackPanel"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/Frame.js",
+        "id": "cordova-plugin-ace.Frame",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.Frame"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/Page.js",
+        "id": "cordova-plugin-ace.Page",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.Page"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/ButtonBase.js",
+        "id": "cordova-plugin-ace.ButtonBase",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.ButtonBase"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/Button.js",
+        "id": "cordova-plugin-ace.Button",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.Button"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/ColumnDefinition.js",
+        "id": "cordova-plugin-ace.ColumnDefinition",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.ColumnDefinition"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/CommandBar.js",
+        "id": "cordova-plugin-ace.CommandBar",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.CommandBar"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/DatePicker.js",
+        "id": "cordova-plugin-ace.DatePicker",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.DatePicker"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/AppBarButton.js",
+        "id": "cordova-plugin-ace.AppBarButton",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.AppBarButton"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/Geometry.js",
+        "id": "cordova-plugin-ace.Geometry",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.Geometry"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/HyperlinkButton.js",
+        "id": "cordova-plugin-ace.HyperlinkButton",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.HyperlinkButton"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/Image.js",
+        "id": "cordova-plugin-ace.Image",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.Image"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/PathSegment.js",
+        "id": "cordova-plugin-ace.PathSegment",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.PathSegment"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/LineSegment.js",
+        "id": "cordova-plugin-ace.LineSegment",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.LineSegment"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/ListBox.js",
+        "id": "cordova-plugin-ace.ListBox",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.ListBox"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/ListBoxItem.js",
+        "id": "cordova-plugin-ace.ListBoxItem",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.ListBoxItem"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/Shape.js",
+        "id": "cordova-plugin-ace.Shape",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.Shape"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/Path.js",
+        "id": "cordova-plugin-ace.Path",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.Path"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/PathFigure.js",
+        "id": "cordova-plugin-ace.PathFigure",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.PathFigure"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/PathGeometry.js",
+        "id": "cordova-plugin-ace.PathGeometry",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.PathGeometry"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/Popup.js",
+        "id": "cordova-plugin-ace.Popup",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.Popup"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/RowDefinition.js",
+        "id": "cordova-plugin-ace.RowDefinition",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.RowDefinition"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/Style.js",
+        "id": "cordova-plugin-ace.Style",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.Style"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/Setter.js",
+        "id": "cordova-plugin-ace.Setter",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.Setter"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/StaticResource.js",
+        "id": "cordova-plugin-ace.StaticResource",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.StaticResource"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/TabBar.js",
+        "id": "cordova-plugin-ace.TabBar",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.TabBar"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/TableView.js",
+        "id": "cordova-plugin-ace.TableView",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.TableView"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/TableViewCell.js",
+        "id": "cordova-plugin-ace.TableViewCell",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.TableViewCell"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/TextBlock.js",
+        "id": "cordova-plugin-ace.TextBlock",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.TextBlock"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/TimePicker.js",
+        "id": "cordova-plugin-ace.TimePicker",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.TimePicker"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/ToggleSwitch.js",
+        "id": "cordova-plugin-ace.ToggleSwitch",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.ToggleSwitch"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/framework/WebView.js",
+        "id": "cordova-plugin-ace.WebView",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.WebView"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/xbf/BinaryReader.js",
+        "id": "cordova-plugin-ace.BinaryReader",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.BinaryReader"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/xbf/NodeType.js",
+        "id": "cordova-plugin-ace.NodeType",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.NodeType"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/xbf/XamlNode.js",
+        "id": "cordova-plugin-ace.XamlNode",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.XamlNode"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/xbf/XbfReader.js",
+        "id": "cordova-plugin-ace.XbfReader",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.XbfReader"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/xbf/NodeToElement.js",
+        "id": "cordova-plugin-ace.NodeToElement",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.NodeToElement"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ace/www/android/platform.js",
+        "id": "cordova-plugin-ace.platform",
+        "pluginId": "cordova-plugin-ace",
+        "merges": [
+            "ace.platform"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+        "id": "cordova-plugin-splashscreen.SplashScreen",
+        "pluginId": "cordova-plugin-splashscreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-globalization/www/GlobalizationError.js",
+        "id": "cordova-plugin-globalization.GlobalizationError",
+        "pluginId": "cordova-plugin-globalization",
+        "clobbers": [
+            "window.GlobalizationError"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-globalization/www/globalization.js",
+        "id": "cordova-plugin-globalization.globalization",
+        "pluginId": "cordova-plugin-globalization",
+        "clobbers": [
+            "navigator.globalization"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-crosswalk-webview": "2.2.0",
+    "cordova-plugin-ace": "0.1.2",
+    "cordova-plugin-splashscreen": "4.0.1",
+    "cordova-plugin-whitelist": "1.3.1",
+    "cordova-plugin-globalization": "1.0.5"
+}
+// BOTTOM OF METADATA
+});
