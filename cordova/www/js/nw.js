ef126673ed66558c1ecbc7a42a12ccd743f8a357
Plasmmer <plasmmer@outlook.com>
2017-06-04 01:39:57 -0300

    Updates
    Signed-off-by: Plasmmer <plasmmer@outlook.com>

---
+++ cordova/www/js/nw.js
@@ -0,0 +1,16 @@
+var gui = require('nw.gui');
+var windowMenu = new gui.Menu({ type: 'menubar' });
+// Add first level menu
+var firstMenuItem = new gui.MenuItem({
+  label:'File',
+  submenu: new gui.Menu()
+});
+windowMenu.append(firstMenuItem);
+
+// Add submenu items
+firstMenuItem.submenu.append(new gui.MenuItem({label:'Open'}));
+firstMenuItem.submenu.append(new gui.MenuItem({label:'Save'}));
+firstMenuItem.submenu.append(new gui.MenuItem({type:'separator'}));
+firstMenuItem.submenu.append(new gui.MenuItem({label:'Exit'}));
+
+gui.Window.get().menu = windowMenu;
